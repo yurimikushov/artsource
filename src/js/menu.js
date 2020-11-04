d001758c53d7f8840d3519a5ef8241a700e9d1db
Yuri Mikushov <mikushov.yu@gmail.com>
2020-11-04 22:07:05 +0300

    feature: implemented handlers to menu buttons

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,25 @@
+// Small screen menu
+
+const menu = document.querySelector('.menu')
+
+const openSmallScreenMenu = () => {
+  menu.classList.add('is-open')
+  document.body.setAttribute('style', 'overflow-y:hidden')
+}
+
+const closeSmallScreenMenu = () => {
+  menu.classList.remove('is-open')
+  document.body.setAttribute('style', 'overflow-y:scroll')
+}
+
+document.querySelector('.burger-menu').addEventListener('click', () => {
+  if (!menu.classList.contains('is-open')) {
+    openSmallScreenMenu()
+  } else {
+    closeSmallScreenMenu()
+  }
+})
+
 // Opacity menu background
 
 setOpacityToMenuBg()
@@ -7,7 +29,7 @@ window.addEventListener('scroll', function () {
 })
 
 function setOpacityToMenuBg() {
-  const opacity = pageYOffset / (contentBlockHeight() - menuBackgroundHeight())
+  const opacity = pageYOffset / (portfolioStartAt() - menuBackgroundHeight())
 
   document.querySelector('.menu-background').style.opacity = opacity
 }
@@ -16,16 +38,20 @@ function menuBackgroundHeight() {
   return document.querySelector('.menu-background').offsetHeight
 }
 
-function contentBlockHeight() {
-  return document.querySelector('#start-screen').offsetHeight
-}
+// Scroll to start
 
-// Scroll to content
+document.querySelector('#to-start').addEventListener('click', () => {
+  smoothScrollTo(0)
+  closeSmallScreenMenu()
+})
 
-const scrollToContentBtn = document.querySelector('.scroll-to-content-btn')
+// Scroll to portfolio
+
+const scrollToContentBtn = document.querySelector('.scroll-to-portfolio-btn')
 
 scrollToContentBtn.addEventListener('click', () => {
-  smoothScrollTo(contentBlockHeight())
+  smoothScrollTo(portfolioStartAt())
+  closeSmallScreenMenu()
 })
 
 function smoothScrollTo(top) {
@@ -33,4 +59,37 @@ function smoothScrollTo(top) {
     top: top - 55,
     behavior: 'smooth',
   })
+}
+
+document.querySelector('#to-portfolio').addEventListener('click', () => {
+  smoothScrollTo(portfolioStartAt())
+  closeSmallScreenMenu()
+})
+
+// Scroll to services
+
+document.querySelector('#to-services').addEventListener('click', () => {
+  smoothScrollTo(servicesStartAt())
+  closeSmallScreenMenu()
+})
+
+// Scroll to contact
+
+document.querySelector('#to-footer').addEventListener('click', () => {
+  smoothScrollTo(footerStartAt())
+  closeSmallScreenMenu()
+})
+
+// Calculate blocks start positions
+
+function portfolioStartAt() {
+  return document.querySelector('#start-screen').offsetHeight
+}
+
+function servicesStartAt() {
+  return portfolioStartAt() + document.querySelector('#portfolio').offsetHeight
+}
+
+function footerStartAt() {
+  return servicesStartAt() + document.querySelector('#services').offsetHeight
 }
