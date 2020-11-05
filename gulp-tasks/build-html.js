4cb29d8896ab2b66b06aa062e052c2f33d3357cb
Yuri Mikushov <mikushov.yu@gmail.com>
2020-11-05 20:25:22 +0300

    refactor: extracted info about portfolio to json

--- gulp-tasks/build-html.js
+++ gulp-tasks/build-html.js
@@ -3,6 +3,7 @@ const cleanDir = require('gulp-clean-dir')
 const pug = require('gulp-pug')
 const htmlValidator = require('gulp-w3c-html-validator')
 const gulpHtmlBemValidator = require('gulp-html-bem-validator')
+const porfolio = require('./../src/data/portfolio.json')
 
 module.exports = function buildHtml() {
   return src('src/pages/*.pug')
@@ -11,7 +12,11 @@ module.exports = function buildHtml() {
         ext: ['.html'],
       })
     )
-    .pipe(pug())
+    .pipe(
+      pug({
+        data: porfolio,
+      })
+    )
     .pipe(htmlValidator())
     .pipe(gulpHtmlBemValidator())
     .pipe(dest('build'))
