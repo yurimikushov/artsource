383aa67bad625b39ca1d93c87141960b1ecbed15
Yuri Mikushov <mikushov.yu@gmail.com>
2020-11-05 15:53:36 +0300

    feature: implemented display portfolio items

--- gulp-tasks/build-img.js
+++ gulp-tasks/build-img.js
@@ -2,7 +2,7 @@ const { src, dest } = require('gulp')
 const cleanDir = require('gulp-clean-dir')
 
 module.exports = function buildImg() {
-  return src('src/images/*.{ico,png,jpg,svg,webp}')
+  return src('src/images/**/*.{ico,png,jpg,svg,webp}')
     .pipe(cleanDir('build/images'))
     .pipe(dest('build/images'))
 }
