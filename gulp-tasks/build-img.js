71addebf3d1dd21f243bf682f31201c4235e601d
Yuri Mikushov <mikushov.yu@gmail.com>
2020-11-07 13:41:10 +0300

    chore: added images minifier

--- gulp-tasks/build-img.js
+++ gulp-tasks/build-img.js
@@ -1,8 +1,10 @@
 const { src, dest } = require('gulp')
 const cleanDir = require('gulp-clean-dir')
+const imagemin = require('gulp-imagemin')
 
 module.exports = function buildImg() {
   return src('src/images/**/*.{ico,png,jpg,svg,webp}')
     .pipe(cleanDir('build/images'))
+    .pipe(imagemin())
     .pipe(dest('build/images'))
 }
