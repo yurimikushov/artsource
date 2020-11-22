6c7945f420fb765bf3307ecc1cc3cb6dd5a14204
Yuri Mikushov <mikushov.yu@gmail.com>
2020-11-22 18:55:49 +0300

    chore: extracted minifying images to Gulp task

--- gulp-tasks/build.js
+++ gulp-tasks/build.js
@@ -1,7 +1,8 @@
 const { series } = require('gulp')
-const buildHtml = require('./build-html')
+const imageMin = require('./image-min')
 const buildImg = require('./build-img')
 const buildCss = require('./build-css')
 const buildJs = require('./build-js')
+const buildHtml = require('./build-html')
 
-module.exports = series(buildImg, buildCss, buildJs, buildHtml)
+module.exports = series(imageMin, buildImg, buildCss, buildJs, buildHtml)
