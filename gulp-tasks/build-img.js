71275c123fc142b66caed75f081911b265d63350
Yuri Mikushov <mikushov.yu@gmail.com>
2020-11-22 19:53:28 +0300

    chore: excluded converted to base64 images

--- gulp-tasks/build-img.js
+++ gulp-tasks/build-img.js
@@ -1,8 +1,19 @@
 const { src, dest } = require('gulp')
 const cleanDir = require('gulp-clean-dir')
 
-module.exports = function buildImg() {
-  return src('src/images/**/*.{ico,png,jpg,svg,webp}')
+const convertedToBase64 = [
+  'src/images/portfolio/**',
+  'src/images/services/**',
+  'src/images/footer/**',
+]
+
+module.exports = () => {
+  return (
+    src(
+      'src/images/**/*.{ico,png,jpg,svg,webp}',
+      { ignore: convertedToBase64 }
+    )
     .pipe(cleanDir('build/images'))
     .pipe(dest('build/images'))
+  )
 }
