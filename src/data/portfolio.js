55d28352bc89c250ee560f526cbea11801fe423f
Yuri Mikushov <mikushov.yu@gmail.com>
2020-11-28 22:53:39 +0300

    refactor: extracted converting images to base64

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,82 +1,84 @@
-module.exports = [
-  {
-    instagram: 'https://www.instagram.com/p/B4kVR5Wgb5l/',
-    src: './src/images/portfolio/magazine.png',
-    discription: 'Разработка фирменного каталога продукции',
-  },
-  {
-    instagram: 'https://www.instagram.com/p/Bxmrn9ZnHFf/',
-    src: './src/images/portfolio/visit.png',
-    discription: 'Разработка дизайн визитки',
-  },
-  {
-    instagram: 'https://www.instagram.com/p/BwrlssnAGz3/',
-    src: './src/images/portfolio/logo-lhc.png',
-    discription: 'Прототипирование и разработка логотипа. Брендинг',
-  },
-  {
-    instagram: 'https://www.instagram.com/p/B2jESoIA6a8/',
-    src: './src/images/portfolio/lis.png',
-    discription: 'Разработка раздаточного флаера',
-  },
-  {
-    instagram: 'https://www.instagram.com/p/BpM9lAHF0IS/',
-    src: './src/images/portfolio/logo-dodydog.png',
-    discription: 'Разработка логотипа',
-  },
-  {
-    instagram: '',
-    src: './src/images/portfolio/logo-owl.png',
-    discription: 'Отрисовка иллюстрации',
-  },
-  {
-    instagram: 'https://www.instagram.com/p/BtdbR_ZAgID/',
-    src: './src/images/portfolio/logo-vrm-group.png',
-    discription: 'Разработка логотипа для группы компаний',
-  },
-  {
-    instagram: 'https://www.instagram.com/p/B1YTwSogW2G/',
-    src: './src/images/portfolio/notebook.png',
-    discription: 'Дизайн макет блокнота',
-  },
-  {
-    instagram: 'https://www.instagram.com/p/Br28QswHF8a/',
-    src: './src/images/portfolio/akademika-baner.png',
-    discription: 'Разработка макета на билборды',
-  },
-  {
-    instagram: 'https://www.instagram.com/p/B4xL3GZgg-N/',
-    src: './src/images/portfolio/preview.png',
-    discription: 'Индивидуальный дизайн визиток',
-  },
-  {
-    instagram: '',
-    src: './src/images/portfolio/booklet-gelpolish.png',
-    discription: 'Разработка евробуклета',
-  },
-  {
-    instagram: 'https://www.instagram.com/p/B5kbQiiAOQ8/',
-    src: './src/images/portfolio/logo-wall.png',
-    discription: 'Разработка фирменного стиля',
-  },
-  {
-    instagram: 'https://www.instagram.com/p/Bt0fnNMgbWQ/',
-    src: './src/images/portfolio/sport-club-akademica.png',
-    discription: 'Баннер на сайт',
-  },
-  {
-    instagram: 'https://www.instagram.com/p/BtLk-TbA2Yd/',
-    src: './src/images/portfolio/kofe.png',
-    discription: 'Концепт бумажных стаканчиков',
-  },
-  {
-    instagram: 'https://www.instagram.com/p/B4Ro85Bg5jQ/',
-    src: './src/images/portfolio/logo-indian.png',
-    discription: 'Прототип логотипа',
-  },
-  {
-    instagram: 'https://www.instagram.com/p/B1N9z-gAWqM/',
-    src: './src/images/portfolio/table-tent-card.png',
-    discription: 'Тейбл тент для кафе и ресторанов',
-  },
-]
+module.exports = {
+  portfolio: [
+    {
+      instagram: 'https://www.instagram.com/p/B4kVR5Wgb5l/',
+      src: './src/images/portfolio/magazine.png',
+      discription: 'Разработка фирменного каталога продукции',
+    },
+    {
+      instagram: 'https://www.instagram.com/p/Bxmrn9ZnHFf/',
+      src: './src/images/portfolio/visit.png',
+      discription: 'Разработка дизайн визитки',
+    },
+    {
+      instagram: 'https://www.instagram.com/p/BwrlssnAGz3/',
+      src: './src/images/portfolio/logo-lhc.png',
+      discription: 'Прототипирование и разработка логотипа. Брендинг',
+    },
+    {
+      instagram: 'https://www.instagram.com/p/B2jESoIA6a8/',
+      src: './src/images/portfolio/lis.png',
+      discription: 'Разработка раздаточного флаера',
+    },
+    {
+      instagram: 'https://www.instagram.com/p/BpM9lAHF0IS/',
+      src: './src/images/portfolio/logo-dodydog.png',
+      discription: 'Разработка логотипа',
+    },
+    {
+      instagram: '',
+      src: './src/images/portfolio/logo-owl.png',
+      discription: 'Отрисовка иллюстрации',
+    },
+    {
+      instagram: 'https://www.instagram.com/p/BtdbR_ZAgID/',
+      src: './src/images/portfolio/logo-vrm-group.png',
+      discription: 'Разработка логотипа для группы компаний',
+    },
+    {
+      instagram: 'https://www.instagram.com/p/B1YTwSogW2G/',
+      src: './src/images/portfolio/notebook.png',
+      discription: 'Дизайн макет блокнота',
+    },
+    {
+      instagram: 'https://www.instagram.com/p/Br28QswHF8a/',
+      src: './src/images/portfolio/akademika-baner.png',
+      discription: 'Разработка макета на билборды',
+    },
+    {
+      instagram: 'https://www.instagram.com/p/B4xL3GZgg-N/',
+      src: './src/images/portfolio/preview.png',
+      discription: 'Индивидуальный дизайн визиток',
+    },
+    {
+      instagram: '',
+      src: './src/images/portfolio/booklet-gelpolish.png',
+      discription: 'Разработка евробуклета',
+    },
+    {
+      instagram: 'https://www.instagram.com/p/B5kbQiiAOQ8/',
+      src: './src/images/portfolio/logo-wall.png',
+      discription: 'Разработка фирменного стиля',
+    },
+    {
+      instagram: 'https://www.instagram.com/p/Bt0fnNMgbWQ/',
+      src: './src/images/portfolio/sport-club-akademica.png',
+      discription: 'Баннер на сайт',
+    },
+    {
+      instagram: 'https://www.instagram.com/p/BtLk-TbA2Yd/',
+      src: './src/images/portfolio/kofe.png',
+      discription: 'Концепт бумажных стаканчиков',
+    },
+    {
+      instagram: 'https://www.instagram.com/p/B4Ro85Bg5jQ/',
+      src: './src/images/portfolio/logo-indian.png',
+      discription: 'Прототип логотипа',
+    },
+    {
+      instagram: 'https://www.instagram.com/p/B1N9z-gAWqM/',
+      src: './src/images/portfolio/table-tent-card.png',
+      discription: 'Тейбл тент для кафе и ресторанов',
+    },
+  ],
+}
--- src/data/services.js
+++ src/data/services.js
@@ -1,48 +1,50 @@
-module.exports = [
-  {
-    src: './src/images/services/innovation.png',
-    title: 'Разработка фирменного стиля',
-    discription: ['Фирменный стиль', 'Брендинг', 'Логотипы'],
-  },
-  {
-    src: './src/images/services/flyers.png',
-    title: 'Полиграфический дизайн',
-    discription: [
-      'Листовки / Евробуклеты',
-      'Флаеры / Афиши',
-      'Визитки / Подарочные сертификаты',
-    ],
-  },
-  {
-    src: './src/images/services/analysis.png',
-    title: 'Отрисовка инфографики',
-    discription: [
-      'Статичная инфорграфика',
-      'Иллюстрация и анимация',
-      'Динамическая инфографика',
-    ],
-  },
-  {
-    src: './src/images/services/open-book.png',
-    title: 'Полиграфическая верстка',
-    discription: [
-      'Маркетинг-кит',
-      'Журналы, каталоги',
-      'Меню для кафе и баров',
-    ],
-  },
-  {
-    src: './src/images/services/banner.png',
-    title: 'Дизайн наружной рекламы',
-    discription: ['Баннеры', 'Вывески', 'Таблички / указатели'],
-  },
-  {
-    src: './src/images/services/browser.png',
-    title: 'Разработка сайтов',
-    discription: [
-      'Одностраничные сайты',
-      'Многостраничные сайты',
-      'E-mail-рассылки',
-    ],
-  },
-]
+module.exports = {
+  services: [
+    {
+      src: './src/images/services/innovation.png',
+      title: 'Разработка фирменного стиля',
+      discription: ['Фирменный стиль', 'Брендинг', 'Логотипы'],
+    },
+    {
+      src: './src/images/services/flyers.png',
+      title: 'Полиграфический дизайн',
+      discription: [
+        'Листовки / Евробуклеты',
+        'Флаеры / Афиши',
+        'Визитки / Подарочные сертификаты',
+      ],
+    },
+    {
+      src: './src/images/services/analysis.png',
+      title: 'Отрисовка инфографики',
+      discription: [
+        'Статичная инфорграфика',
+        'Иллюстрация и анимация',
+        'Динамическая инфографика',
+      ],
+    },
+    {
+      src: './src/images/services/open-book.png',
+      title: 'Полиграфическая верстка',
+      discription: [
+        'Маркетинг-кит',
+        'Журналы, каталоги',
+        'Меню для кафе и баров',
+      ],
+    },
+    {
+      src: './src/images/services/banner.png',
+      title: 'Дизайн наружной рекламы',
+      discription: ['Баннеры', 'Вывески', 'Таблички / указатели'],
+    },
+    {
+      src: './src/images/services/browser.png',
+      title: 'Разработка сайтов',
+      discription: [
+        'Одностраничные сайты',
+        'Многостраничные сайты',
+        'E-mail-рассылки',
+      ],
+    },
+  ],
+}
