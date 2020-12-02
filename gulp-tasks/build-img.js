cbaee66f89739d803cb37de13e6c0f53c3834080
Yuri Mikushov <mikushov.yu@gmail.com>
2020-12-02 17:44:55 +0300

    chore: added converting img to base64 only for prod

--- gulp-tasks/build-img.js
+++ gulp-tasks/build-img.js
@@ -1,16 +1,20 @@
 const { src, dest } = require('gulp')
 const cleanDir = require('gulp-clean-dir')
 
-const convertedToBase64 = [
-  'src/images/portfolio/**',
-  'src/images/services/**',
-  'src/images/footer/**',
-]
+let convertedToBase64 = []
+
+if (process.env.NODE_ENV == 'prod') {
+  convertedToBase64 = [
+    'src/images/portfolio/**',
+    'src/images/services/**',
+    'src/images/footer/**',
+  ]
+}
 
 module.exports = function buildImg() {
   return src('src/images/**/*.{ico,png,jpg,svg,webp}', {
-    ignore: convertedToBase64,
-  })
+      ignore: convertedToBase64,
+    })
     .pipe(cleanDir('build/images'))
     .pipe(dest('build/images'))
 }
